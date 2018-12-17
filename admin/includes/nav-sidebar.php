8f93d74e8ab849bc8957da91061e852fa5c28fac
Hoang Trung <itlabviet@gmail.com>
2018-12-17 10:53:52 +0700

    .

--- admin/includes/nav-sidebar.php
+++ admin/includes/nav-sidebar.php
@@ -35,6 +35,12 @@
                     <li> <a href="user_create.php">Thêm mới</a> </li>
                 </ul>
             </li>
+            <li> <a href="#" class="waves-effect active"><i class="linea-icon linea-basic fa-fw" data-icon="v"></i> <span class="hide-menu"> Quản trị Danh mục <span class="fa arrow"></span></span></a>
+                <ul class="nav nav-second-level">
+                    <li> <a href="category_index.php">Danh sách</a> </li>
+                    <li> <a href="category_create.php">Thêm mới</a> </li>
+                </ul>
+            </li>
         </ul>
     </div>
 </div>
