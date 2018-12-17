1b35c893114200c5d054b7d103bf4d45875ac0d9
ptmlhue <ptmylinhhue@gmail.com>
2018-12-17 11:53:32 +0700

    category_index

--- admin/category_create.php
+++ admin/category_create.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     require_once('class.db.php');
     $database = new DB();
 
-    $database->insert('groups', $_POST);
+    $database->insert('category', $_POST);
 }
 ?>
 
@@ -30,12 +30,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="container-fluid">
                 <div class="row bg-title">
                     <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                        <h4 class="page-title">Thêm mới nhóm</h4> </div>
+                        <h4 class="page-title">Thêm mới danh mục</h4> </div>
                     <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12"> 
                         <!-- <a href="https://themeforest.net/item/elite-admin-the-ultimate-dashboard-web-app-kit-material-design/16750820?ref=suniljoshi" target="_blank" class="btn btn-danger pull-right m-l-20 btn-rounded btn-outline hidden-xs hidden-sm waves-effect waves-light">Buy Now</a> -->
                         <ol class="breadcrumb">
                             <li><a href="#">Trang chủ</a></li>
-                            <li><a href="#">Danh sách nhóm</a></li>
+                            <li><a href="#">Danh sách danh mục</a></li>
                         </ol>
                     </div>
                     <!-- /.col-lg-12 -->
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <div class="white-box">
                             <form class="form" method="POST">
                                 <div class="form-group row">
-                                    <label class="col-2 col-form-label">Tên nhóm</label>
+                                    <label class="col-2 col-form-label">Tên danh mục</label>
                                     <div class="col-10">
                                         <input class="form-control" type="text" name="name">
                                     </div>
